288dd95dee067be38f5be6fc7ef3b9b1b50745a2
Nix <rabbitnix@postech.ac.kr>
2026-03-24 02:36:47 +0900

    [Update] [CLI] 퀴즈용 훈음 전처리과정 추가

--- src/auxiliary.py
+++ src/auxiliary.py
@@ -37,7 +37,8 @@ def get_repr_pairs_user(hanja: dict) -> list[str]:
     try:
         # 어문회 제공 훈에 식별을 위해 한자 서술을 포함하는 경우,
         # 이를 제거한 '사용자가 쉽게 입력할 수 있는' 형태의 훈음-의미 쌍을 반환
-        # ex) "성(姓)" -> "성"
+        # ex) "성(姓) 강 姜" -> "성 강 姜"
+        # ex) "일[首荷] 대 戴" -> "일 대 戴"
         meaning_parse = ast.literal_eval(hanja['meaning_user'])
     except KeyError:
         # 'meaning_user' 필드가 없는 경우, 기존 'meaning' 필드를 사용
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,3 +1,4 @@
+import ast
 import csv
 import json
 
@@ -13,6 +14,31 @@ def preprocess(data_dir: str = "data"):
         item['grade'] = level_dict_kor[item['level']]
         item['selected'] = False  # 초기값 설정
         item['selected_ever'] = False  # 초기값 설정
+
+        meaning_parse = ast.literal_eval(item['meaning'])
+        meaning_user_parse = []; required = False
+        for meaning in meaning_parse:
+            _m = meaning[0]
+            _s = meaning[1]; assert len(_s) == 1, "invalid format"
+
+            meaning_user_parse.append([[], _s])
+            for __m in _m:
+                # 어문회 제공 훈에 식별을 위해 한자 서술을 포함하는 경우,
+                # 이를 제거한 '사용자가 쉽게 입력할 수 있는' 형태의 훈음-의미 쌍을 생성
+                # ex) "성(姓) 강 姜" -> "성 강 姜"
+                # ex) "일[首荷] 대 戴" -> "일 대 戴"
+                if __m.endswith(']'):
+                    ___m = __m.split('[')[0]
+                    meaning_user_parse[-1][0].append(___m)
+                    required = True
+                elif __m.endswith(')'):
+                    ___m = __m.split('(')[0]
+                    meaning_user_parse[-1][0].append(___m)
+                    required = True
+                else:
+                    meaning_user_parse[-1][0].append(__m)
+        if required:
+            item['meaning_user'] = str(meaning_user_parse)
 
     with open(f'{data_dir}/hanja.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
